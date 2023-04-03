20957515feecba5f60d236eacd548fc96ece9ac2
rshourou <52804311+rshourou@users.noreply.github.com>
2023-04-03 16:12:06 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,5 +17,5 @@ terraform {
 provider "aws" {
   region                   = "us-east-1"
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "Credentials"
+  shared_config_files      = ["~/.aws/config"]
 }
