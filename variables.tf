da8bc1c81e740b89bedf0b2a82657ab054b4a195
rshourou <52804311+rshourou@users.noreply.github.com>
2023-04-03 16:24:16 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+    type = string
+    description = "The size of the EC2 instance"
+    default = "t2.micro"
+    sensitive = false
+
+    validation {
+      condition = can(regex("^t2.", var.instance_type))
+      error_message = "The instance must be a t2 type EC2 instance"    
+    }
+}
+variable "region" {
+    type = string 
+    default = "us-east-1
+}
+
+variable "availability_zones" {
+    type = string  
+}
