bfc6029d1a1f9aa76ed584fc1839dc0a262a141b
rshourou <52804311+rshourou@users.noreply.github.com>
2023-04-03 16:44:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 
 locals {
   proj_name = "dev"
+   ingress_rules= [
+    {
+      port= 22,
+      description ="SSH" },
+    {
+      port= 80,
+      description ="HTTP"
+    }
+  ]
 }
 
 resource "aws_vpc" "mtc_vpc" {
@@ -51,13 +60,17 @@ resource "aws_security_group" "mtc_sg" {
   name        = "${local.proj_name}_sg"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.mtc_vpc.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description      = ingress.value.description
+      from_port        = ingress.value.port
+      to_port          = ingress.value.port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+    } 
   }
 
   egress {
